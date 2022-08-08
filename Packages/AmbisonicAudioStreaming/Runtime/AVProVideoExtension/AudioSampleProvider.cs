a2e4628d0ab44f5d080fc78cdcb9d34d7d0b8d70
Soichiro Sugimoto <16936357+sotanmochi@users.noreply.github.com>
2022-08-08 12:55:32 +0900

    Update AudioSampleProvider

--- Packages/AmbisonicAudioStreaming/Runtime/AVProVideoExtension/AudioSampleProvider.cs
+++ Packages/AmbisonicAudioStreaming/Runtime/AVProVideoExtension/AudioSampleProvider.cs
@@ -11,23 +11,39 @@ namespace AmbisonicAudioStreaming.AVProVideoExtension
         Ambisonics = 4,
     }
 
-    public class AudioSampleProvider : MonoBehaviour
+    public class AudioSampleProvider : MonoBehaviour, IAudioSampleProvider
     {
 		[SerializeField] MediaPlayer _mediaPlayer = null;
         [SerializeField] ChannelType _outputChannelType = ChannelType.Ambisonics;
-        [SerializeField] uint _sampleFrameCount = 4096; 
+        [SerializeField] uint _bufferSampleCount = 4096; 
 
-        public event Action<uint, ushort, float[]> OnAudioSampled;
+        public event Action OnPrepared;
+        public event AudioFrameSampledEventHandler OnAudioSampled;
 
         private IMediaControl _mediaControl = null;
-        private uint _outputChannelCount;
+        private ushort _outputChannelCount;
+        private float[] _outputPcmBuffer = null;
         private float[] _pcmBuffer = null;
 
         void Awake()
         {
             _mediaControl = _mediaPlayer.Control;
-            _outputChannelCount = (uint)_outputChannelType;
-            _pcmBuffer = new float[_sampleFrameCount * _outputChannelCount];
+            _outputChannelCount = (ushort)_outputChannelType;
+
+            _outputPcmBuffer = new float[_bufferSampleCount * _outputChannelCount];
+            _pcmBuffer = new float[_bufferSampleCount * _outputChannelCount];
+
+            OnPrepared?.Invoke();
+        }
+
+        void Start()
+        {
+
+        }
+
+        void OnDestroy()
+        {
+            
         }
 
         void Update()
@@ -46,16 +62,14 @@ namespace AmbisonicAudioStreaming.AVProVideoExtension
         public void UpdateAudio()
         {
             var channelCount = (ushort)_mediaControl.GetAudioChannelCount();
+            // Debug.Log($"UpdateAudio.ChannelCount: {channelCount}");
 
             if (channelCount <= 0)
             {
                 return;
             }
 
-            // Debug.Assert(_pcmBuffer.Length % _outputChannelCount == 0);
-            // Debug.Assert(channelCount == _outputChannelCount);
-
-            var requiredBufferSize = _sampleFrameCount * channelCount;
+            var requiredBufferSize = _bufferSampleCount * channelCount;
             if (_pcmBuffer is null || _pcmBuffer.Length != requiredBufferSize)
             {
                 _pcmBuffer = new float[requiredBufferSize];
@@ -68,7 +82,27 @@ namespace AmbisonicAudioStreaming.AVProVideoExtension
 #endif
             if (samples != 0)
             {
-                OnAudioSampled?.Invoke(_sampleFrameCount, channelCount, _pcmBuffer);
+                uint readPos = 0;
+                uint writePos = 0;
+
+                var lesserChannels = Math.Min(channelCount, _outputChannelCount);
+
+                // Debug.Log($"channelCount: {channelCount}");
+                // Debug.Log($"OutputChannelCount: {_outputChannelCount}");
+                // Debug.Log($"LesserChannels: {lesserChannels}");
+
+                for (int i = 0; i < _bufferSampleCount; ++i)
+                {
+                    for (int j = 0; j < lesserChannels; ++j)
+                    {
+                        _outputPcmBuffer[writePos + j] = _pcmBuffer[readPos + j];
+                    }
+
+                    readPos += channelCount;
+                    writePos += _outputChannelCount;
+                }
+
+                OnAudioSampled?.Invoke(_bufferSampleCount, _outputChannelCount, _outputPcmBuffer);
             }
         }
     }
